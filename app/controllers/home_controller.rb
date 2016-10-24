04e73b782d38afe6bb0892be2e3286a1933d15d5
krishnakarthik1309 <krishnakarthik1309@gmail.com>
2016-10-24 18:00:14 +0530

    add coursera courses to db

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,38 @@
 require 'net/http'
 require 'json'
+require 'db/CourseTable.rb'
 
 class HomeController < ApplicationController
   def catalog
-  	coursera_url = 'https://api.coursera.org/api/courses.v1?start=0&limit=25'
-  	coursera_uri = URI(courses_url)
-  	coursera_response = Net::HTTP.get(courses_uri)
-  	coursera_json = JSON.parse(courses_response)
+  	p = PostgresDirect.new()
+  	p.connect
+  	begin
+	    p.createUserTable("catalog")
+	    p.prepareInsertUserStatement("catalog")
+
+	    i = 0
+	    len = 1
+	    while len > 0 do
+	    	coursera_url = 'https://api.coursera.org/api/courses.v1?start=#{i}&limit=25'
+		  	coursera_uri = URI(coursera_url)
+		  	coursera_response = Net::HTTP.get(coursera_uri)
+		  	coursera_json = JSON.parse(coursera_response)
+		  	coursera_courses = coursera_json["elements"]
+		  	
+		  	# add each course to db
+		  	for course in coursera_courses
+			  	id = course["id"]
+			  	name = course["name"]
+			  	slug = course["slug"]
+			  	courseType = course["courseType"] 
+			  	p.addUser(id, name, slug, courseType)
+		  	end
+
+		  	len = coursera_courses.length
+		  	i += 25
+	    end
+	end
 
   end
 end
+require '/home/kk/Dropbox/Acads/sem 5/CS252/coursebucket/db/CourseTable'
