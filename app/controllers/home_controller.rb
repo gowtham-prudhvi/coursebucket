f593d802b2fe1db4523fdc9e5322062e404ac264
krishnakarthik1309 <krishnakarthik1309@gmail.com>
2016-10-25 15:09:35 +0530

    move add_courses func to HomeHelper

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,42 +9,12 @@ class HomeController < ApplicationController
   	p = PostgresDirect.new()
   	p.connect
   	
-  	begin
-	    p.createUserTable("catalog")
-	    p.prepareInsertUserStatement("catalog")
+    p.createUserTable("catalog")
+    p.prepareInsertUserStatement("catalog")
 
-	    for course_site in HomeHelper::MOOCS
-	    	add_courses_to_db(course_site)
-		end
+    for course_site in HomeHelper::MOOCS
+    	HomeHelper.add_courses_to_db(course_site, p)
 	end
   end
-
-  def add_courses_to_db(course_site)
-  	i = 0
-    nxt = 1
-    while nxt > 0 do
-    	course_url = HomeHelper.get_url(course_site, i)
-	  	course_uri = URI(course_url)
-	  	course_response = Net::HTTP.get(course_uri)
-	  	course_json = JSON.parse(course_response)
-	  	course_courses = HomeHelper.get_courses(course_json)
-	  	
-	  	# add each course to db
-	  	for course in course_courses
-		  	id = HomeHelper.get_id(course)
-		  	name = HomeHelper.get_name(course)
-		  	slug = HomeHelper.get_slug(course)
-		  	p.addUser(id, name, slug, course_site)
-	  	end
-
-	  	i += 25
-	  	if course_site == "coursera"
-	  		nxt = course_json["next"]
-	  	else
-	  		nxt = 0
-	  	end
-    end
-  end
-
-
+  
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,6 +1,33 @@
 module HomeHelper
 	MOOCS = {"coursera", "udacity"}
 
+	def add_courses_to_db(course_site, connection)
+	  	i = 0
+	    nxt = 1
+	    while nxt > 0 do
+	    	course_url = get_url(course_site, i)
+		  	course_uri = URI(course_url)
+		  	course_response = Net::HTTP.get(course_uri)
+		  	course_json = JSON.parse(course_response)
+		  	course_courses = get_courses(course_json)
+		  	
+		  	# add each course to db
+		  	for course in course_courses
+			  	id = get_id(course)
+			  	name = get_name(course)
+			  	slug = get_slug(course)
+			  	connection.addUser(id, name, slug, course_site)
+		  	end
+
+		  	i += 25
+		  	if course_site == "coursera"
+		  		nxt = course_json["next"]
+		  	else
+		  		nxt = 0
+		  	end
+	    end
+  	end
+
 	def get_courses(json_data)
 		if json_data.key?(:elements)
 			return json_data["elements"]
